1ab6ca26cb6e6ea1393f0ac5f82f11d6199e42c9
wbget <157354966@qq.com>
2025-02-12 18:52:15 +0800

    新增 lodash 依赖并更新 ATSService 中的相关方法以支持更灵活的查询选项

--- src/service/ats.service.ts
+++ src/service/ats.service.ts
@@ -1,9 +1,18 @@
 import { Init, Inject, Provide, Scope, ScopeEnum } from '@midwayjs/core';
 import { InjectDataSource } from '@midwayjs/typeorm';
 import { UUIDIntService } from '@wbget/midway-uuid-int';
-import { DataSource, EntityManager, EntityTarget, In } from 'typeorm';
-import { Trait } from '../interface';
+import { isString } from 'lodash';
+import {
+  DataSource,
+  EntityManager,
+  EntityTarget,
+  FindManyOptions,
+  FindOneOptions,
+  FindOptionsWhere,
+  In,
+} from 'typeorm';
 import { AtomEntity } from '../entity/ats.entity';
+import { Trait } from '../interface';
 
 @Provide()
 @Scope(ScopeEnum.Singleton)
@@ -45,25 +54,27 @@ export class ATSService {
   ) {
     await this.manager.delete(trait, { id });
   }
+  async getTrait<Entity extends Trait>(trait: EntityTarget<Entity>, id: string);
   async getTrait<Entity extends Trait>(
     trait: EntityTarget<Entity>,
-    id: string
+    options: FindOneOptions<Entity>
+  );
+  async getTrait<Entity extends Trait>(
+    trait: EntityTarget<Entity>,
+    options: FindOneOptions<Entity> | string
   ) {
-    return (await this.manager.findOne<Trait>(trait, {
-      where: { id },
-    })) as Entity;
+    if (isString(options)) {
+      return this.manager.findOne<Entity>(trait, {
+        where: { id: options },
+      } as FindOneOptions<Entity>);
+    }
+    return this.manager.findOne<Entity>(trait, options);
   }
   async getTraits<Entity extends Trait>(
     trait: EntityTarget<Entity>,
-    ids: string[] = null
+    options: FindManyOptions<Entity> = null
   ) {
-    if (ids === null) {
-      return this.manager.find(trait);
-    } else {
-      return (await this.manager.find<Trait>(trait, {
-        where: { id: In(ids) },
-      })) as Entity[];
-    }
+    return this.manager.find<Entity>(trait, options);
   }
 
   /**
@@ -71,24 +82,39 @@ export class ATSService {
    * @param traits 按顺序过滤，传参数时请按照从最细到最宽的顺序传递
    * @returns
    */
-  async getAtoms<Entity extends Trait>(traits: EntityTarget<Entity>[]) {
+  async getAtoms<Entity extends Trait>(
+    traits: EntityTarget<Entity>[],
+    options: FindOptionsWhere<Entity>[] = null
+  ) {
     if (traits.length === 0) {
       return [];
     }
     // TODO 加速查找
     const atoms: Entity[] = await this.manager.find(traits[0], {
+      where: options ? options[0] : null,
       select: ['id'],
     });
     let ids = atoms.map(atom => atom.id);
     if (traits.length === 1) {
       return ids;
     }
-    for await (const entity of traits.slice(1)) {
-      const tids = await this.manager.find<Trait>(entity, {
-        select: ['id'],
-        where: { id: In(ids) },
-      });
-      ids = tids.map(id => id.id);
+    const others = traits.slice(1);
+    let i = 0;
+    for await (const entity of others) {
+      if (options) {
+        const option = options[i + 1];
+        const tids = await this.manager.find<Trait>(entity, {
+          select: ['id'],
+          where: option ? [{ id: In(ids) }, option] : { id: In(ids) },
+        });
+        ids = tids.map(id => id.id);
+      } else {
+        const tids = await this.manager.find<Trait>(entity, {
+          select: ['id'],
+          where: { id: In(ids) },
+        });
+        ids = tids.map(id => id.id);
+      }
     }
     return ids;
   }
